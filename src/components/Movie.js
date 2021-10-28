fe96b137b6b926aa3b685db7350029c74ac3f038
Your Name <you@example.com>
2021-10-27 22:20:26 -0300

    nuevo

---
+++ src/components/Movie.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+//config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+
+//components
+import BreadCrumb from './BreadCrumb';
+import Grid from './Grid';
+import Spinner from './Spinner';
+import MovieInfo from './MovieInfo';
+import MovieInfoBar from './MovieInfoBar';
+import Actor from './Actor';
+
+//Hook
+import { useMovieFetch } from '../hooks/useMovieFetch';
+
+//Image
+import NoImage from '../images/no_image.jpg';
+
+const Movie = () => {
+    const { movieId } = useParams();
+    const { state: movie, loading, error } = useMovieFetch(movieId);
+    
+    if(loading) return <Spinner />;
+    if(error) return <div>Somenthing went wrong...</div>;
+
+    return (
+        <>
+            <BreadCrumb movieTitle={movie.original_title} />
+            <MovieInfo movie={movie} />
+            <MovieInfoBar 
+                time={movie.runtime} 
+                budget={movie.budget} 
+                revenue={movie.revenue} 
+            />
+            <Grid header='Actors'>
+                {movie.actors.map(actor => (
+                    <Actor 
+                        key={actor.credit_id}
+                        name={actor.name}
+                        character={actor.character}
+                        imageUrl={
+                            actor.profile_path 
+                            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                            : NoImage
+                        }
+                    ></Actor>
+                ))}
+            </Grid>
+        </>
+    );
+};
+
+export default Movie;
